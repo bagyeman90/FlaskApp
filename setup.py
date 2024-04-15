4e1efa1f9189dd4a93581179b033d55feb5a3a64
Benneh Agyeman-Dua <131815160+bagyeman90@users.noreply.github.com>
2024-04-15 19:00:54 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,5 @@
 from setuptools import  setup, find_packages
 
-"""
-This is a docstring for setting up the setuptools.
-By importing setup and to find packages.
-"""
+
 
 setup(name='gusml', version='1.0', packages=find_packages())
