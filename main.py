7f37356ddd5d76f84f023b41d53890c5b6ba58f4
Benneh Agyeman-Dua <131815160+bagyeman90@users.noreply.github.com>
2024-04-15 18:57:08 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 from flask import Flask
 
+""" 
+This is a docstring for the main module.
+It describes what the module does.
+"""
 app = Flask(__name__)
 
 @app.route('/')
 @app.route('/index')
 def index():
+    """
+    This is a docstring for the index function.
+    It describes what the function does,
+    its parameters (if any), and its return value (if any).
+    """
     return  '<p>Hello, World!</p>'
